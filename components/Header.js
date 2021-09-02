2fcc8bf2d9acf8f8f5045ac74fb12d16115ba4db
Blender Defender <defenderblender@gmail.com>
2021-09-02 21:23:06 +0200

    Append " · Super Addon Manager" onto each page title.

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ title }) => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <script src="./bootstrap/bootstrap.bundle.min.js"></script>
 
-    <title>{title}</title>
+    <title>{title + " · Super Addon Manager"}</title>
     <link
       href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap"
       rel="stylesheet"
