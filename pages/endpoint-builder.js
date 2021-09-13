cf7a64346d30c714a25101becbdba1f99a3c5347
Chris Bond <BeheadedKamikaze@users.noreply.github.com>
2021-09-14 05:18:07 +1000

    Stub Endpoint Builder page

---
+++ pages/endpoint-builder.js
@@ -0,0 +1,19 @@
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+
+const EndpointBuilderPage = () => {
+  return (
+    <>
+      <Header title="Endpoint Builder" />
+      <Navbar />
+
+      <div className="container intro">
+        <h1>Endpoint Builder</h1>
+
+        <p>TODO</p>
+      </div>
+    </>
+  )
+};
+
+export default EndpointBuilderPage;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Link from "next/link";
+import { Nav } from "react-bootstrap";
 
 export default function Home() {
   return (
@@ -8,9 +9,18 @@ export default function Home() {
       <Header title="Home" />
       <Navbar />
       <div className="container intro">
-        <Link href="/request-support">
-          Request Support
-        </Link>
+        <Nav as="ul" className="flex-column">
+          <Nav.Item as="li">
+            <Nav.Link as={Link} href="/request-support">
+              Request Support
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link as={Link} href="/endpoint-builder">
+              Endpoint Builder
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
       </div>
     </>
   );
