7f37a38594997f68e9b442833781ae915005d9e4
Blender Defender <defenderblender@gmail.com>
2021-09-18 17:18:30 +0200

    expand_all: Expand all categroies temporarily when searching.

--- functions/main_functions.py
+++ functions/main_functions.py
@@ -5,8 +5,15 @@ from .. import prefs
 
 def expand_all(self, context: Context):
     ''' Property callback to expand all categories in the preferences when searching '''
-    for category_name in list(prefs.expanded_categories):
-        prefs.expanded_categories[category_name] = True
+    for category in list(prefs.filtered_categories):
+        prefs.filtered_categories[category] = False
+
+    if not self.error_search_term:
+        return
+
+
+    for addon in filter(lambda el: filter_search(el, self.error_search_term), prefs.unavailable_addons):
+        prefs.filtered_categories[addon["issue_type"]] = True
 
 
 def filter_search(el, search_term: str) -> bool:
