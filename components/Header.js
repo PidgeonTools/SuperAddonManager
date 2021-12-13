7082039f7d6fdbf27f19bed3a0bdac9116c469b7
Blender Defender <defenderblender@gmail.com>
2021-12-13 16:04:12 +0100

    Head: Add Meta Tags for SEO (commented out and added noindex to keep the website private at the moment.)

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Head from "next/head";
 
-const Header = ({ title }) => (
+const Header = ({ title, description = "" }) => (
   <Head>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    {/* <meta name="author" content="Blender Defender,Chris Bond,Kevin Lorengel" />
+    <meta name="description" content={description} /> */}
+    <meta name="robots" content="noindex" />
 
     <title>{title + " · Super Addon Manager"}</title>
     <link
