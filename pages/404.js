5daeb3a0cdd271c76c22efb41ce4d0ef121cef83
Blender Defender <defenderblender@gmail.com>
2021-12-25 14:43:41 +0100

    404 Page: Finish 404 Page

--- pages/404.js
+++ pages/404.js
@@ -47,22 +47,28 @@ const Custom404 = ({ funFacts, pages, addonDownloadLinks }) => {
           {/* DOWNLOAD LINKS */}
           <div className="error-404-message--link-container error-404-message--text">
             {addonDownloadLinks.map((addon) => (
-              // <Col md={6} lg={4}>
               <>
                 <Link key={addon.name} href={addon.link}>
                   <a>{addon.name}</a>
                 </Link>{" "}
               </>
-              // </Col>
             ))}
           </div>
           {/* FUN FACT */}
           <div className="error-404-message--text">Fun Fact: {funFact}</div>
         </Container>
+        {/* BOTTOM WAVES */}
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 315">
+          <path
+            fill="#ffffff"
+            fillOpacity="1"
+            d="M0,160L34.3,144C68.6,128,137,96,206,112C274.3,128,343,192,411,229.3C480,267,549,277,617,234.7C685.7,192,754,96,823,90.7C891.4,85,960,171,1029,197.3C1097.1,224,1166,192,1234,170.7C1302.9,149,1371,139,1406,133.3L1440,128L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
+          ></path>
+        </svg>
       </section>
 
       {/* CONTACT LINK */}
-      <div>
+      <div className="error-404-message--text error-404-message--link-container error-404-message--contact">
         You are 100% sure this page should be here?{" "}
         <Link href="https://discord.gg/hGzHDA7bj9">
           <a target="_blank" rel="noopener noreferrer">
