957ba5d213873333b8420ec4a7e9f179657b407e
Blender Defender <defenderblender@gmail.com>
2022-01-29 16:03:52 +0100

    Documentation Image: Ensure that all images have an alt attribute, allow additional classNames.

--- components/docs/DocumentationImage.js
+++ components/docs/DocumentationImage.js
@@ -6,16 +6,21 @@ const DocumentationImage = ({
   language = "Placeholder",
   localeVersions = "en",
   fileExtension = "png",
+  alt = "",
+  className,
   ...props
 }) => {
   const basePath = "/images/docs";
   const locale = localeVersions.split(";").includes(language) ? language : "en";
 
   const src = `${basePath}/${locale}/${article}/${filename}.${fileExtension}`;
+
+  className = className ? " " + className : ""
+  const finalProps = {...props, src, alt, className: "img-fluid" + className}
   return (
     <>
       <div className="docs-img-wrapper">
-        <img {...props} src={src} className="img-fluid" />
+        <img {...finalProps} />
       </div>
     </>
   );
