cc708227fd5a822e5c0f383636fcd0521e213166
Blender Defender <defenderblender@gmail.com>
2022-02-14 16:15:04 +0100

    Fix build error

--- pages/settings.js
+++ pages/settings.js
@@ -4,32 +4,34 @@ import React, { useState, useEffect } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 // Bootstrap
-import { Dropdown } from "react-bootstrap";
+import { Dropdown, Form } from "react-bootstrap";
 
 // Languages
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import { locales } from "../lib/i18n/getLocaleData";
 import { getLanguage, setLanguage } from "../functions";
 
 // Theme
-import { setTheme, THEMES } from "../functions/getTheme";
+import { getTheme, setTheme, THEMES } from "../functions/getTheme";
 import IntlWrapper from "../components/IntlWrapper";
 
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-  <div
-    ref={ref}
-    className="sam-toggle-button"
-    onClick={(e) => {
-      e.preventDefault();
-      onClick(e);
-    }}
-  >
-    {children}
-  </div>
-));
+//// const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+////   <div
+////     ref={ref}
+////     className="sam-toggle-button"
+////     onClick={(e) => {
+////       e.preventDefault();
+////       onClick(e);
+////     }}
+////   >
+////     {children}
+////   </div>
+//// ));
 
 const Settings = () => {
+  const intl = useIntl();
   const [language, setLanguageHook] = useState("en");
+  const [theme, setThemeHook] = useState("light");
   const [minifyEndpointData, setMinifyEndpointData] = useLocalStorage(
     "minify_endpoint_data",
     false
@@ -37,53 +39,94 @@ const Settings = () => {
 
   useEffect(() => {
     setLanguageHook(getLanguage(window));
+    setThemeHook(getTheme(window));
   }, []);
 
   return (
     <IntlWrapper>
       <div>Settings</div>
-      <Dropdown className="theme-switcher">
-        <Dropdown.Toggle as={CustomToggle}>
-          <span>asdf</span>
-        </Dropdown.Toggle>
-        <Dropdown.Menu aria-labelledby="dropdownMenuButton1">
-          {Object.keys(THEMES).map((key) => {
-            let theme = THEMES[key];
-            return (
-              <Dropdown.Item
-                key={theme}
-                onClick={(e) => {
-                  document.documentElement.className = theme.class;
-                  setTheme(theme.class, window);
-                }}
-              >
-                <FormattedMessage id={theme.id} />
-              </Dropdown.Item>
-            );
-          })}
-        </Dropdown.Menu>
-      </Dropdown>
-      <Dropdown className="language-switcher">
-        <Dropdown.Toggle as={CustomToggle}>
-          <span>{locales[language].flag}</span>
-        </Dropdown.Toggle>
-        <Dropdown.Menu aria-labelledby="dropdownMenuButton1">
-          {Object.keys(locales).map((key) => {
-            let locale = locales[key];
-            return (
-              <Dropdown.Item
-                key={locale}
-                onClick={(e) => {
-                  setLanguage(locale.locale, window);
-                  location.reload();
-                }}
-              >
-                {locale.flag} <FormattedMessage id={locale.id} />
-              </Dropdown.Item>
-            );
-          })}
-        </Dropdown.Menu>
-      </Dropdown>
+      {/* THEME */}
+      <Form.Select
+        className="sam-form-select"
+        onChange={({ target }) => {
+          document.documentElement.className = target.value;
+          setTheme(target.value, window);
+          setThemeHook(target.value);
+        }}
+        value={theme}
+      >
+        {Object.keys(THEMES).map((key) => {
+          let theme = THEMES[key];
+          return (
+            <option key={theme} value={theme.class}>
+              {intl.formatMessage({ id: theme.id })}
+            </option>
+          );
+        })}
+      </Form.Select>
+
+      {/* LANGUAGE */}
+      <Form.Select
+        className="sam-form-select"
+        value={language}
+        onChange={({ target }) => {
+          setLanguage(target.value, window);
+          location.reload();
+        }}
+      >
+        {Object.keys(locales).map((key) => {
+          let locale = locales[key];
+          return (
+            <option key={locale} value={locale.locale}>
+              {locale.flag} {intl.formatMessage({ id: locale.id })}
+            </option>
+          );
+        })}
+      </Form.Select>
+      {
+        /////* <Dropdown className="theme-switcher">
+        ////<Dropdown.Toggle as={CustomToggle}>
+        ////        <span>asdf</span>
+        ////      </Dropdown.Toggle>
+        ////      <Dropdown.Menu aria-labelledby="dropdownMenuButton1">
+        ////        {Object.keys(THEMES).map((key) => {
+        ////          let theme = THEMES[key];
+        ////          return (
+        ////            <Dropdown.Item
+        ////              key={theme}
+        ////              onClick={(e) => {
+        ////                document.documentElement.className = theme.class;
+        ////                setTheme(theme.class, window);
+        ////              }}
+        ////            >
+        ////              <FormattedMessage id={theme.id} />
+        ////            </Dropdown.Item>
+        ////          );
+        ////        })}
+        ////      </Dropdown.Menu>
+        ////    </Dropdown>
+        ////    <Dropdown className="language-switcher">
+        ////      <Dropdown.Toggle as={CustomToggle}>
+        ////        <span>{locales[language].flag}</span>
+        ////      </Dropdown.Toggle>
+        ////      <Dropdown.Menu aria-labelledby="dropdownMenuButton1">
+        ////        {Object.keys(locales).map((key) => {
+        ////          let locale = locales[key];
+        ////          return (
+        ////            <Dropdown.Item
+        ////              key={locale}
+        ////              onClick={(e) => {
+        ////                setLanguage(locale.locale, window);
+        ////                location.reload();
+        ////              }}
+        ////            >
+        ////              {locale.flag} <FormattedMessage id={locale.id} />
+        ////            </Dropdown.Item>
+        ////          );
+        ////        })}
+        ////      </Dropdown.Menu>
+        ////    </Dropdown> */
+      }
       <div className="form-check">
         <input
           type="checkbox"
