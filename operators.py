bcba7007ba06cc6d97c9e2931fcd2e89c963dacc
Blender Defender <contact.blenderdefender@gmail.com>
2023-04-13 15:04:20 +0200

    chore: :page_facing_up: Update GPL License blocks in all Python files/add missing GPL license blocks

--- operators.py
+++ operators.py
@@ -1,7 +1,7 @@
 # ##### BEGIN GPL LICENSE BLOCK #####
 #
 #  <A central Updater for all your Addons>
-#    Copyright (C) <2021>  <Blender Defender>
+#    Copyright (C) <2023>  <Blender Defender>
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
